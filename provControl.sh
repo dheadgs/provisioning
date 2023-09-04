409009515d982d7d09d31b722bce528f1a07aa86
ansible <shms@space.gr>
2023-09-04 16:07:28 +0300

    Changed report.py to yearly also

--- provControl.sh
+++ provControl.sh
@@ -8,7 +8,7 @@
 #                                                       #
 #########################################################
 
-functio restart()
+function restart()
 {
 provisioner=`pgrep -lf provisioner | head -n1 | cut -d' ' -f1`
 notification=`pgrep -lf notification | head -n1 | cut -d' ' -f1`
